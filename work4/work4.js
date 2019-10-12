7b552cdd371302b3068946d0dcec6ca700203309
Lixiaobai1907 <1617446859@qq.com>
2019-10-12 18:54:39 +0800

    work4

--- work4/work4.js
+++ work4/work4.js
@@ -1,8 +1,8 @@
 document.write("<p>金字塔</p>")
-let level = prompt('请设置金字塔层数')
+let level = prompt('请设置金字塔的层数')
 level = parseFloat(level)&&Number(level)
 if(isNaN(level)){
-    alert('金字塔的层数数字')
+    alert('金字塔的层数是数字')
 }
 for(let i=1;i<=level;++i){
     let blank = level - i;
@@ -15,11 +15,11 @@ for(let i=1;i<=level;++i){
     }
     document.write("<br>")
 }
-let str = '<table bordere="1">'
+let str = '<table border="1">'
 for(let i=1;i<10;++i){
     str += '<tr>'
     for(var j=1;j<=i;++j){
-        str +='<td>' + j + '*' + i + '=' + ( j * i )
+        str +='<td>' + j + '*' + i + '=' + ( j * i ) + '</td>'
     }
     str += '<tr/>'
 
